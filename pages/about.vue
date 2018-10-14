61ef23aad94b0ace3c994fe89d0fc166ff4c6807
Maksim Ivanov <mak.ivanov@gmail.com>
2018-10-14 22:29:39 +0300

    added changes - fire to api

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>About page</div>
+  <div class="posts-container">
+    <div v-for="post in posts" 
+      :key="post.id"
+      class="post-box">
+      <h1>{{post.title}}</h1>
+      <p>{{post.body}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  async asyncData ({ params }) {
+    let { data }  = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    console.log(data)
+    return { posts: data }
+  }
 }
 </script>
 
 <style>
-
+.posts-container{
+  width: 1000px;
+  margin: 0 auto;
+}
+.post-box {
+  margin-bottom: 10px;
+}
 </style>
