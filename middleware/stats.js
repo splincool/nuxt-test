165591752ad54c8a3ce3460a8a56af880015450c
Maksim Ivanov <mak.ivanov@gmail.com>
2018-10-15 22:37:38 +0300

    added a plugin

--- middleware/stats.js
+++ middleware/stats.js
@@ -1,11 +1,5 @@
 
 export default function ({ route }) {
   return {
-      apiKey: 'AIzaSyB9A-GCVY3zKUJKRXroe8h5wuYdDSw2ylQ',
-      authDomain: 'blogvuejs-ed9af.firebaseapp.com',
-      databaseURL: 'https://blogvuejs-ed9af.firebaseio.com',
-      projectId: 'blogvuejs-ed9af',
-      storageBucket: 'blogvuejs-ed9af.appspot.com',
-      messagingSenderId: '700880399834'
   }
 }
