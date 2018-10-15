2592ba1ca9238809b4309f6f9785c7b94ffc2cc2
Maksim Ivanov <mak.ivanov@gmail.com>
2018-10-15 20:25:34 +0300

    added test data

--- middleware/stats.js
+++ middleware/stats.js
@@ -1,7 +1,11 @@
-import axios from 'axios'
 
 export default function ({ route }) {
-  return axios.get('https://jsonplaceholder.typicode.com/posts', {
-    url: route.fullPath
-  })
+  return {
+      apiKey: 'AIzaSyB9A-GCVY3zKUJKRXroe8h5wuYdDSw2ylQ',
+      authDomain: 'blogvuejs-ed9af.firebaseapp.com',
+      databaseURL: 'https://blogvuejs-ed9af.firebaseio.com',
+      projectId: 'blogvuejs-ed9af',
+      storageBucket: 'blogvuejs-ed9af.appspot.com',
+      messagingSenderId: '700880399834'
+  }
 }
